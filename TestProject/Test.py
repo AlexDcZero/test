0ff8ececdd260163288fa1ac135bf8a4b86f7e6d
Alex <Alex@DESKTOP-8L8F0JH>
2019-10-01 15:53:52 +0200

    Second commit

--- TestProject/Test.py
+++ TestProject/Test.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+class Clase:
+    def __init__(self, nombre):
+        self.name = nombre
+    
+    def imprimir(self):
+        print("Tu nombre es " + self.name)
+    
+ejemplo = Clase("Alejandro")
+ejemplo.imprimir()
